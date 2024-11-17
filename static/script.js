164847b85d98a198d30cad038693bc842ffb6e02
dedatekk <dedatekk@gmail.com>
2024-11-17 20:17:48 +1000

    apparently the JS had to change

--- static/script.js
+++ static/script.js
@@ -13,7 +13,7 @@ document.addEventListener("DOMContentLoaded", async () => {
 
     // Fetch the list of hero images from the server
     try {
-        const response = await fetch('http://<EC2_PUBLIC_IP>:8080/hero-images'); // Update to your Flask API
+        const response = await fetch('http://ec2-3-80-195-148.compute-1.amazonaws.com:8080/hero-images'); // Update to your Flask API
         heroImages = await response.json();
 
         if (heroImages.length > 0) {
@@ -48,7 +48,7 @@ document.addEventListener("DOMContentLoaded", async () => {
     let galleryImages = [];
 
     try {
-        const response = await fetch('http://<EC2_PUBLIC_IP>:8080/gallery-images'); // Update to your Flask API
+        const response = await fetch('http://ec2-3-80-195-148.compute-1.amazonaws.com:8080/gallery-images'); // Update to your Flask API
         galleryImages = await response.json();
     } catch (error) {
         console.error("Error fetching gallery images:", error);
