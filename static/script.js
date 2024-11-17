7d9e6e5e0e176e789f3703f37f3ca931a92a9d66
dedatekk <dedatekk@gmail.com>
2024-11-17 20:39:53 +1000

    stil serving images

--- static/script.js
+++ static/script.js
@@ -13,7 +13,7 @@ document.addEventListener("DOMContentLoaded", async () => {
 
     // Fetch the list of hero images from the server
     try {
-        const response = await fetch('http://ec2-3-80-195-148.compute-1.amazonaws.com:8080/hero-images'); // Update to your Flask API
+        const response = await fetch('http://ec2-54-160-225-229.compute-1.amazonaws.com:8080/hero-images'); // Update to your Flask API
         heroImages = await response.json();
 
         if (heroImages.length > 0) {
